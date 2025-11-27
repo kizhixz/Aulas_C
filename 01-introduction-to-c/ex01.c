ec83cfcb6d798cb37f70a6ef8a7aa2f56304c25e
kizhixz <kizhixz@gmail.com>
2025-11-27 10:52:21 -0300

    Corrige a documentação do Exercício 1 e remove o código do Exercício 3

--- 01-introduction-to-c/ex01.c
+++ 01-introduction-to-c/ex01.c
@@ -1,11 +1,11 @@
-"""
-Exercício 1: O Programa Mínimo Crie o esqueleto de um 
-programa em C que não faz nada além de retornar ao sistema operacional, 
-utilizando a diretiva #include para a biblioteca padrão de entrada e saída. 
-A função main deve receber o argumento void.
+/*
+ * Exercício 1: O Programa Mínimo
+ * Crie o esqueleto de um programa em C que não faz nada além de retornar
+ * ao sistema operacional. Utilize a diretiva #include para a biblioteca
+ * padrão de entrada e saída. A função main deve receber o argumento void.
+ */
 
-"""
-include <stdio.h>
+#include <stdio.h>
 
 int main(void) {
     return 0;
