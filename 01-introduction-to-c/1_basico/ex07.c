f8018e044cf9645ae4dd5e95a6cef5cdc80bbd60
kizhixz <kizhixz@gmail.com>
2025-12-01 13:50:19 -0300

    Remove exercícios de introdução à linguagem C e adiciona esboço para manipulação de structs na mini agenda em C.

--- 01-introduction-to-c/1_basico/ex07.c
+++
@@ -1,8 +0,0 @@
-/*
-
-Exercício 7: O Comando de Execução Após a compilação bem-sucedida 
-do Exercício 6, qual é o comando que você digitaria no terminal para rodar o programa?
-
-./ex06
-
-*/
---
+++ projetos_praticos/Tarefa_Agenda_em_C/ex03.c
@@ -0,0 +1,22 @@
+/*
+(3) Structs
+
+Fundamental para criar:
+
+struct Contato { ... }
+
+
+E manipular vários contatos.
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    int x = 10;
+    char nome = [v, b, m];
+
+
+}
+
